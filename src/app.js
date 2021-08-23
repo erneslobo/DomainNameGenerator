a83cb32baf11b4807245c1d90698df0a898014d3
erneslobo <erneslobo@gmail.com>
2021-08-23 02:24:59 +0000

    initial commit by Ernesto

--- src/app.js
+++ src/app.js
@@ -9,3 +9,29 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".us", ".io", "etc"];
+
+let createDomain = (pronoun, adj, noun, extension) => {
+  let randompronoun = Math.floor(Math.random() * pronoun.length);
+  let randomadj = Math.floor(Math.random() * adj.length);
+  let randomnoun = Math.floor(Math.random() * noun.length);
+  let randomextension = Math.floor(Math.random() * extension.length);
+  return (
+    pronoun[randompronoun] +
+    adj[randomadj] +
+    noun[randomnoun] +
+    extension[randomextension]
+  );
+};
+
+document.getElementById("domain").innerHTML = createDomain(
+  pronoun,
+  adj,
+  noun,
+  extension
+);
+console.log(createDomain(pronoun, adj, noun, extension));
