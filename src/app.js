e876fba566f8c0a71b0d6759b48e594e8bc2dd5e
erneslobo <erneslobo@gmail.com>
2021-08-23 15:45:18 +0000

    added for to print all possible domains

--- src/app.js
+++ src/app.js
@@ -16,16 +16,24 @@ let createDomain = () => {
   let noun = ["jogger", "racoon"];
   let extension = [".com", ".net", ".us", ".io", ".org"];
 
-  let randompronoun = Math.floor(Math.random() * pronoun.length);
-  let randomadj = Math.floor(Math.random() * adj.length);
-  let randomnoun = Math.floor(Math.random() * noun.length);
-  let randomextension = Math.floor(Math.random() * extension.length);
-  return (
-    pronoun[randompronoun] +
-    adj[randomadj] +
-    noun[randomnoun] +
-    extension[randomextension]
-  );
+  let ul = "";
+
+  // let randompronoun = Math.floor(Math.random() * pronoun.length);
+  // let randomadj = Math.floor(Math.random() * adj.length);
+  // let randomnoun = Math.floor(Math.random() * noun.length);
+  // let randomextension = Math.floor(Math.random() * extension.length);
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let h = 0; h < extension.length; h++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[h]);
+          ul += "<h2>" + pronoun[i] + adj[j] + noun[k] + extension[h] + "</h2>";
+        }
+      }
+    }
+  }
+  return ul;
 };
 
 document.getElementById("domain").innerHTML = createDomain();
